5d6267fe16fc77c6a1d151591b0c3d1cb864ef08
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-04-07 16:48:02 +0200

    added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <div class="footer-top">
+
+        </div>
+        <div class="footer-bottom">
+
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+name: "pageFooter",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+
+footer{
+    
+    .footer-top{
+        background-color: $bg-footer-grey;
+        height: 45vh;
+    }
+    .footer-bottom{
+        background-color: $bg-footer-dark-grey;
+        height: 12vh;
+    }
+}
+</style>
